2a30709410be159583c7ad4c1d65ee65c70af3d7
vargaluca <114932694+vargaluca@users.noreply.github.com>
2023-08-02 10:38:02 +0200

    added fastq download file

---
+++ code/bash_scripts/download_fastq.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+echo download path:
+read path
+echo run number:
+read run
+echo download project number:
+read project
+
+mkdir -p $path/raw_reads/$run/undetermined
+
+/disk/work/shared/tools/bs/bs project download --id $project -o $path/raw_reads/$run
+mv $path/raw_reads/$run/Undetermined* $path/raw_reads/$run/undetermined/
+
+cd $path/raw_reads/$run
+
+	for folder in PBL* TIS*
+		do
+		mv $folder/* .
+	done
+
+rmdir *
+echo download finished on $(date)
+
+
