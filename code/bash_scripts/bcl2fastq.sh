3c4131f3ea93685d6bee4be8e7f14914e824adaa
vargaluca <114932694+vargaluca@users.noreply.github.com>
2023-08-15 16:32:00 +0200

    added undetermined folder to script

--- code/bash_scripts/bcl2fastq.sh
+++ code/bash_scripts/bcl2fastq.sh
@@ -5,12 +5,14 @@ read path
 echo run number:
 read run
 
-mkdir -p $path/raw_reads/$run
+mkdir -p $path/raw_reads/$run/undetermined
 
 /disk/work/shared/tools/bcl2fastq/bin/bcl2fastq \
         --input-dir $path/bcl_files/$run/Data/Intensities/BaseCalls \
         --runfolder-dir $path/bcl_files/$run \
         --output-dir $path/raw_reads/$run \
         --sample-sheet $path/bcl_files/$run/aml_all*SampleSheet_v2.csv
+
+mv $path/raw_reads/$run/Undetermined* $path/raw_reads/$run/undetermined/
 
 echo fastq generated for $run on $(date)
