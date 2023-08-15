e3f6cb7f3ed67b025d83348ba44b5def904c13da
vargaluca <114932694+vargaluca@users.noreply.github.com>
2023-08-15 14:50:21 +0200

    added maf to xlsx file with checks

---
+++ code/bash_scripts/twist_maf_to_xlsx.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+echo path to folders:
+read path
+echo run number:
+read run
+
+mkdir -p $path/tables/$run/xlsx
+cd $path/maf_files/$run
+
+Rscript='/disk/work/shared/tools/R/bin/Rscript'
+xlsx='/disk/work/users/lv1/twist_myeloid_lymphoid/code/R'
+
+for maffile in *.maf.gz
+	do
+	name=$(echo $maffile | cut -f 1 -d ".")
+	zcat $maffile > $path/tables/$run/$(echo $maffile | cut -f 1 -d ".").table
+	$Rscript $xlsx/table_to_xlsx.R $path/tables/$run/$name.table $path/tables/$run/xlsx/$name.xlsx
+
+	if [ -f $path/tables/$run/xlsx/$name.xlsx ]
+	then
+		if [ -s $path/tables/$run/xlsx/$name.xlsx ]
+		then
+			echo "$name.xlsx successfully created"
+		else
+			echo "$name.xlsx exists but is empty"
+		fi
+	else
+		echo "$name.xlsx doesn't exist"
+	fi
+done
