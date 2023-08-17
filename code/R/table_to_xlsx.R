ec23d9684e891bf44ab690f50c24e5c3812164e2
vargaluca <114932694+vargaluca@users.noreply.github.com>
2023-08-17 13:30:17 +0200

    added maf processing and gene filtering steps to xlsx R script

--- code/R/table_to_xlsx.R
+++ code/R/table_to_xlsx.R
@@ -1,8 +1,24 @@
 library(openxlsx)
+library(dbplyr)
+library(tidyverse)
+library(maftools)
 
 args <- commandArgs(trailingOnly = T)
 file <- args[1]
 outfile <- args[2]
 
-data <- read.delim(file, comment.char = "#", sep = "\t", na.strings = "NA")
-write.xlsx(data, outfile)
+maffile <- read.maf(file)
+gene_csv <- read_csv(
+  "/disk/work/diagnostics/TWLM/lv1/twist_all_aml_diagnostics/bed_files/Twist_myeloid+lymphoid_genelist.csv", 
+  col_names = T)
+genelist <- gene_csv$gene
+
+filtered_maf <- subsetMaf(maffile, genes = genelist)
+maf_data <- filtered_maf@data
+maf_silent <- filtered_maf@maf.silent
+maf_all <- rbind(maf_data, maf_silent, fill = T)
+  
+maf_all <- maf_all %>%
+  mutate(VAF = t_alt_count / t_depth * 100)
+
+write.xlsx(maf_all, outfile)
